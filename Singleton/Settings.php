8ca29b3936dea33e04ef7d4859f3e8acbd18c14b
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-07-05 16:57:53 +0300

    enabled psr12

--- Singleton/Settings.php
+++ Singleton/Settings.php
@@ -4,37 +4,39 @@ namespace Singleton;
 
 final class Settings
 {
-	private static $object = null;
+    private static $object = null;
 
-	private $settings;
+    private $settings;
 
-	private function __construct()
-	{
-		$settings = [];
-	}
+    private function __construct()
+    {
+        $settings = [];
+    }
 
-	private function __clone(){}
+    private function __clone()
+    {
+    }
 
-	public static function get()
-	{
-		if(is_null(self::$object)) {
-			self::$object = new self();
-		}
+    public static function get()
+    {
+        if (is_null(self::$object)) {
+            self::$object = new self();
+        }
 
-		return self::$object;
-	}
+        return self::$object;
+    }
 
-	public function __get($key)
-	{
-		if(array_key_exists($key, $this->settings)){
-			return $this->settings[$key];
-		} else {
-			return null;
-		}
-	}
+    public function __get($key)
+    {
+        if (array_key_exists($key, $this->settings)) {
+            return $this->settings[$key];
+        } else {
+            return null;
+        }
+    }
 
-	public function __set($key, $value)
-	{
-		$this->settings[$key] = $value;
-	}
+    public function __set($key, $value)
+    {
+        $this->settings[$key] = $value;
+    }
 }
